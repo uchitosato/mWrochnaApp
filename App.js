3d86bfdccadbce7756a8906289b860b7e0c1b62e
uchitosato <uchitosato@gmail.com>
2023-06-14 23:09:49 +0900

    refactor code

--- App.js
+++ App.js
@@ -1,8 +1,5 @@
 import React from 'react';
-
-//import all the components we are going to use
 import { SafeAreaView } from 'react-native';
-
 import { WebView } from 'react-native-webview';
 
 const App = () => {
